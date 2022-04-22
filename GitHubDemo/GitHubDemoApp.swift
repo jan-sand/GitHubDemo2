e5603be34276a1ae316bdb9fed6574f7bbadc76b
jan-sand <102095212+jan-sand@users.noreply.github.com>
2022-04-22 20:48:57 +0200

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by JBS on 22.04.22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
